294d06b7aa96f5c5882ddf8c4a7452fa223d9a2e
Fredrik Wollsén <fredrik@neam.se>
2014-04-03 23:31:42 +0200

    Provisioning of memcache, nodejs, yeoman and mosh

--- provision.sh
+++ provision.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+cd /root
+mkdir provisioning
+cd provisioning
+
 apt-get update
-apt-get install -y -q
-npm
+
+apt-get install -y -q memcached
+
+apt-get install -y -q sudo
+
+git clone https://github.com/neam/provisioning-shell-scripts.git
+
+cd provisioning-shell-scripts/debian-wheezy/
+
+./nodejs.sh
+
+npm install -g grunt-cli bower yo
+
+./mosh.sh
+
+cd /root
